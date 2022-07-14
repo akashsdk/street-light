15e308c15fa5410ce6a57a541c0b877f1138146a
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-14 18:10:03 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './screen/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
      <Home/>
+     <Footer/>
     </div>
   );
 }
