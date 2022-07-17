6f308037406340294ae7b628c928d3c01cb6d664
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-17 22:05:01 +0600

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,22 @@ import { PageHeader } from 'antd';
 export default function Header() {
   return (
     <div>
-    <PageHeader
-    className="site-page-header"
-    onBack={() => null}
-    title="Title"
-    subTitle="This is a subtitle"
-
-    
-  />
-  <text>gfhgfgfgfghghjvghv</text>
-  </div>
+      <div className='headerDiv'>
+        <PageHeader
+          className="headerPageheader"
+          onBack={() => null}
+          title="Smart Street Lighting System"
+          subTitle=""
+        />
+        <nav>
+          <a className="link-1" href="#">Home</a>
+          <a className="link-1" href="#">System</a>
+          <a className="link-1" href="#">About</a>
+          <a className="link-1" href="#">Contact</a>
+          
+        </nav>
+      </div>
+      <div className='HeaderLineDiv'></div>
+    </div>
   )
 }
