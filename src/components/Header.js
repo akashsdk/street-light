a7726dfb7f0a27157f3fec563c10f00e85a806c0
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-19 01:10:53 +0600

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,25 @@ import { PageHeader } from 'antd';
 import System from "../screen/System"
 import About from '../screen/About';
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+
+
 export default function Header() {
   return (
     <div>
       <div className='headerDiv'>
-        <PageHeader
-          className="headerPageheader"
-          onBack={() => null}
-          title="Smart Street Lighting System"
-          subTitle=""
-        />
+        <div className='headerDivUp'>
+          <PageHeader
+            className="headerPageheader"
+            onBack={() => null}
+            title="Smart Street Lighting System"
+            subTitle=""
+          />
+          <button className='headerButton'>
+            <AiOutlineUser className='headerIcon' />
+            <p>Akash</p>
+          </button>
+        </div>
         <nav>
           <Link className="link-1" to="/">Home</Link>
           <Link className="link-1" to="/System">System</Link>
