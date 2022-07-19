acaaea5123de6916199cedb05dcba0f8019c2d8f
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-20 04:29:01 +0600

    ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,19 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='FooterPage'>
-      <div className='FooterLineDive'>
+      <div className='FooterLineDive'></div>
+      <div className='FooterBody'>
+        <div>
+          <h1>1ts div</h1>
+        </div>
+        <div>
+          <h1>2ts div</h1>
+        </div>
+        <div>
+          <h1>3ts div</h1>
+        </div>
       </div>
     </div>
   )
 }
+
