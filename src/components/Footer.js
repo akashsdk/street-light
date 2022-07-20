b0e4696f4381f5d8ffae6d2c6fede86ef0acc396
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-20 11:48:25 +0600

    ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import Light from '../Image/Street Light.png'
+import LightLeft from '../Image/Street light2.png'
 
 export default function Footer() {
   return (
@@ -12,9 +13,12 @@ export default function Footer() {
         </div>
         <div className='FooterScendDiv'>
           <div className='FooterScendLeft'>
-          <img src={Light} className="footerImage" />
+            <img src={Light} className="footerImage" />
           </div>
           <div>right</div>
+          <div className='FooterScendLeft'>
+            <img src={LightLeft} className="footerImage" />
+          </div>
         </div>
       </div>
     </div>
