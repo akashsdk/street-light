0e237133382a6ccbe0370b03ab3327cb864941d7
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-21 19:24:21 +0600

    ok

--- src/Card/LightCart.js
+++ src/Card/LightCart.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './LightCart.css'
+import CardLight from '../Image/CardLight.png'
+import Switch from "react-switch";
+
 
 export default function LightCart() {
-  return (
-    <div className='LightCartBody'>LightCart</div>
-  )
+    const [value,setValue]=React.useState()
+    return (
+        <div className='LightCartBody'>
+            <p className='LightCartBodyP'>Street Light</p>
+            <div className='LightCartBox'>
+                <div>
+                    <img src={CardLight} className="LightCartImg" />
+                </div>
+                <div>
+                <Switch onChange={setValue} checked={value} />
+                </div>
+            </div>
+            <div className='LightCartDownDiv'>
+                <p className='LightCartDownP'>Acctive</p>
+            </div>
+        </div>
+    )
 }
--- src/screen/System.js
+++ src/screen/System.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/System.css'
+import LightCart from '../Card/LightCart'
 
 export default function System() {
   return (
     <div className='SystemBody'>
-      System
+      <LightCart/>
     </div>
   )
 }
