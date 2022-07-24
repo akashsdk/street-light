6da257212f75bbb63ff847ef7e100273df927eca
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-25 01:48:10 +0600

    ok

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Index.css"
+import ImgMani from '../Image/StreetMaintenance.jpeg'
 
 export default function Index() {
   return (
     <div className='indexBody'>
         <p className='indexFistP'>Street Light Maintenance</p>
+        <img src={ImgMani} className="indexImgMani" />
     </div>
   )
 }
