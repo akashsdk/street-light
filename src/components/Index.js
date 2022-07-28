efaf4bb748c9f5b7fdeb80e6aac73fba97db5dc6
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-07-28 23:43:40 +0600

    ok

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./Index.css"
 import ImgMani from '../Image/StreetMaintenance.jpeg'
+import CircuitDesign from '../Image/CircuitDesign.png'
 
-import { Col, Row } from 'antd';
+import { Col, Row, Pagination } from 'antd';
 
 
 
@@ -21,13 +22,20 @@ export default function Index() {
           <p className='indexFistPText'>Street lighting is usually owned by counties or cities and is seen in neighbourhoods. Our Street Lighting Maintenance crews furnish all labour, materials, and equipment and supervise the maintenance of many street lighting systems in our service area. Repairs include but are not limited to the lamp, ballast and photocell repair and replacement; installation of light pole and/or luminaries arms; complete fixture replacement, and cleaning or repair of components. Our crews perform street light installation, street light maintenance and repair of conduit, junction boxes, conduit bores, grounding; installation and repair to concrete foundations; locating, installing, testing, and splicing conductor cables; and modifications to service panels as required.</p>
 
         </Col>
-        
+
       </Row>
+      <div className='indecLineDive'></div>
 
       <div>
         <p className='indexFistP'>street light circuit design</p>
+        <img src={CircuitDesign} className='indexImgeCircuit' />
       </div>
+      <div className='indecLineDive'></div>
 
+      <div className='indecNoticeDiv'>
+        <h5>Notice</h5>
+        <Pagination defaultCurrent={1} total={50} />
+      </div>
 
 
     </div>
