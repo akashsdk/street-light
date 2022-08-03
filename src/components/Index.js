95a0d2502e9cff6a555c5a5e3d6a64d383ff8de5
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-08-04 03:55:58 +0600

    ok

--- src/components/Index.js
+++ src/components/Index.js
@@ -8,6 +8,7 @@ import { Col, Row, Pagination } from 'antd';
 
 
 export default function Index() {
+  const [page,setPage]=React.useState(1)
   return (
     <div className='indexBody'>
       <Row>
@@ -35,10 +36,25 @@ export default function Index() {
       <div className='indecNoticeDiv'>
         <h5>Notice Board</h5>
         <div className='indecNoticeBox'>
-          <p className='indecNoticeText'>upcoming notice</p>
-          <div>fgh</div>
+          {
+            page==1?(
+              <p className='indecNoticeText'>upcoming notice</p>
+            ):page==2?(
+              <p className='indecNoticeText'>upcoming notice 2</p>
+            ):page==3?(
+              <p className='indecNoticeText'>upcoming notice 3</p>
+            ):page==4?(
+              <p className='indecNoticeText'>upcoming notice 4</p>
+            ):page==5?(
+              <p className='indecNoticeText'>upcoming notice 5</p>
+            ):(
+              <p className='indecNoticeText'>Error</p>
+            )
+          }
         </div>
-        <Pagination defaultCurrent={1} total={50} />
+        <Pagination onChange={(page,pageSize)=>{
+          setPage(page)
+        }} defaultCurrent={1} current={page} total={50} />
       </div>
 
 
