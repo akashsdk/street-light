9fbe8acb764ef45ee0fdf81d4f52ee59e9c8aec4
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-08-18 05:51:33 +0600

    ok

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Input, Button, Modal } from 'antd';
-import "./LogIn.css"
+import "./LogIn.css";
+import './ChangePassword.css'
 import app from '../firebase'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { VscEyeClosed, VscEye } from "react-icons/vsc";
+import { AiFillStar } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import MovingText from 'react-moving-text'
 
 const { confirm } = Modal;
@@ -25,6 +29,8 @@ const showConfirm = (msg) => {
 };
 
 export default function LogIn() {
+    const [hide, setHide] = React.useState(false);
+
     const [email, setEmail] = React.useState()
     const [password, setPassword] = React.useState()
     const auth = getAuth(app)
@@ -47,30 +53,51 @@ export default function LogIn() {
 
 
     return (
-        <div>
-            <div>
-                <div>
-                
-                </div>
-            </div>
-            
+        <div className='LogINBody'>
+
             <div>
                 <h1>Log In</h1>
                 <h3>
                     user ID: "someone@gmail.com", {"\n"}
                     Password: "123456"
                 </h3>
-                <div>
-                    <Input onChange={(e) => {
-                        setEmail(e.target.value)
-                    }} className='logInInput' placeholder="UserID" />
-                    <Input onChange={(e) => {
-                        setPassword(e.target.value)
-                    }} className='logInInput' placeholder="Password" />
 
-                    <Button onClick={login} type="primary">Log In</Button>
-                </div>
             </div>
+
+            <div className='LogInBox'>
+
+                <h2 className="changeh2">User ID <AiFillStar className="changeStarIcon" /></h2>
+                <div className="changeInputDiv">
+                    <input onChange={(e) => {
+                        setEmail(e.target.value)
+                    }} type='text' className="logInInput"
+                        placeholder="User ID" />
+                </div>
+
+
+                <h2 className="changeh2">Password <AiFillStar className="changeStarIcon" /></h2>
+                <div className="changeInputDiv">
+                    <input onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} type={hide ? "password" : 'text'}
+                        className="logInInput" placeholder="Password" />
+                    <a style={{
+                        textDecoration: 'none'
+                    }} onClick={() => setHide(!hide)}>
+                        {
+                            hide ? (<VscEyeClosed />) : (<VscEye />)
+                        }
+                    </a>
+                </div>
+
+                <button onClick={login} type="submit" className="chanheSubmit" >
+                    <h1 className="chanheSubmitH1">Log In</h1>
+                </button>
+
+                <div className="chanheDownDiv"></div>
+            </div>
+
+
         </div>
     )
 }
