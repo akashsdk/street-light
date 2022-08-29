44adbd390814d6b3c30dafd8d2685117b36ec428
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-08-29 20:21:32 +0600

    ok

--- src/Admin Panel/Try.js
+++ src/Admin Panel/Try.js
@@ -1,7 +1,91 @@
 import React from 'react'
+import ProfilePhoto from "../Image/Pfofilephoto.jpeg"
+import { AiOutlinePaperClip, AiOutlineLogout, AiFillEdit } from "react-icons/ai";
+import Loading from "../Card/Loading.js"
+
+
+import { getAuth, signOut } from "firebase/auth";
+import app from "../firebase";
+import { Button } from 'antd';
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { onSnapshot, doc, getFirestore, updateDoc } from 'firebase/firestore';
+import { getDownloadURL, getStorage, uploadBytes, ref } from 'firebase/storage'
+
+
+
+
 
 export default function Try() {
-  return (
-    <div>Try</div>
-  )
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    const storage = getStorage(app)
+    const storageRef = ref(storage, `notice/${auth.currentUser.uid}.jpg`)
+    const [file, setFile] = React.useState(ProfilePhoto)
+
+    const [user, setUser] = React.useState(null)
+    const domRef = React.useRef()
+
+
+
+    const unsub = onSnapshot(doc(db, "user_data", auth.currentUser.uid), (doc) => {
+        setUser(doc.data())
+    });
+    const metadata = {
+        contentType: 'notice/jpeg',
+    };
+    const upload = (data) => {
+        if (!data) {
+            console.log('select a file')
+            return
+        }
+        uploadBytes(storageRef, data, metadata).then(() => {
+            console.log('success')
+            download()
+        })
+    }
+    const download = () => {
+        getDownloadURL(storageRef).then(url => {
+            updateDoc(doc(db, 'user_data', auth.currentUser.uid), {
+                notice: url
+            }).then(() => {
+                console.log('Database updated successfull')
+            }).catch(err => {
+                console.log(err.message)
+            })
+        })
+    }
+    const fileToDataUri = (file) => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            resolve(event.target.result)
+        };
+        reader.readAsDataURL(file);
+    })
+
+    React.useEffect(() => {
+        unsub()
+    }, [])
+    return (
+        <div>
+            {user ? (<></>) : (<Loading />)}
+            <button> Uplode</button>
+            <div className='mainProfileImgDiv'>
+                        <img src={user && user.notice ? user.notice : ProfilePhoto} className='mainProfileImg'>
+                        </img>
+                        <div onClick={() => {
+                            domRef.current.click()
+                        }} className='mainProfileIconDiv'>
+                            <AiOutlinePaperClip className='mainProfileIcon' />
+                        </div>
+                        <input ref={domRef} onChange={(e) => {
+                            console.log(e.target.files)
+                            fileToDataUri(e.target.files[0]).then(url => {
+                                setFile(url)
+                            })
+
+                            upload(e.target.files[e.target.files.length - 1])
+                        }} style={{ display: 'none' }} accept='notice/jpg' type='file' name='file'></input>
+                    </div>
+        </div>
+    )
 }
--- src/screen/MainProfile.js
+++ src/screen/MainProfile.js
@@ -10,20 +10,20 @@ import { getAuth, signOut } from "firebase/auth";
 import app from "../firebase";
 import { Button } from 'antd';
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
-import { onSnapshot,doc,getFirestore,updateDoc } from 'firebase/firestore';
-import {getDownloadURL,getStorage,uploadBytes,ref} from 'firebase/storage'
+import { onSnapshot, doc, getFirestore, updateDoc } from 'firebase/firestore';
+import { getDownloadURL, getStorage, uploadBytes, ref } from 'firebase/storage'
 
 
 
 export default function MainProfile() {
     const auth = getAuth(app);
-    const db=getFirestore(app);
-    const storage=getStorage(app)
-    const storageRef=ref(storage,`image/${auth.currentUser.uid}.jpg`)
-    const [file,setFile]=React.useState(ProfilePhoto)
+    const db = getFirestore(app);
+    const storage = getStorage(app)
+    const storageRef = ref(storage, `image/${auth.currentUser.uid}.jpg`)
+    const [file, setFile] = React.useState(ProfilePhoto)
 
-    const [user,setUser]=React.useState(null)
-    const domRef=React.useRef()
+    const [user, setUser] = React.useState(null)
+    const domRef = React.useRef()
     const logout = () => {
         signOut(auth).then(() => {
             // Sign-out successful.
@@ -37,32 +37,32 @@ export default function MainProfile() {
     });
     const metadata = {
         contentType: 'image/jpeg',
-      };
-    const upload=(data)=>{
-        if(!data){
+    };
+    const upload = (data) => {
+        if (!data) {
             console.log('select a file')
             return
         }
-        uploadBytes(storageRef,data,metadata).then(()=>{
-        console.log('success')
-        download()
-    })
-}
-    const download=()=>{
-        getDownloadURL(storageRef).then(url=>{
-            updateDoc(doc(db,'user_data',auth.currentUser.uid),{
-                image:url
-            }).then(()=>{
+        uploadBytes(storageRef, data, metadata).then(() => {
+            console.log('success')
+            download()
+        })
+    }
+    const download = () => {
+        getDownloadURL(storageRef).then(url => {
+            updateDoc(doc(db, 'user_data', auth.currentUser.uid), {
+                image: url
+            }).then(() => {
                 console.log('Database updated successfull')
-            }).catch(err=>{
-                       console.log(err.message)
-              })
+            }).catch(err => {
+                console.log(err.message)
+            })
         })
     }
     const fileToDataUri = (file) => new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.onload = (event) => {
-          resolve(event.target.result)
+            resolve(event.target.result)
         };
         reader.readAsDataURL(file);
     })
@@ -74,14 +74,14 @@ export default function MainProfile() {
     //     }).catch(err=>{
     //         console.log(err.message)
     //     })
-       
+
     // })
-    React.useEffect(()=>{
+    React.useEffect(() => {
         unsub()
-    },[])
+    }, [])
     return (
         <div>
-             {user?(<></>):(<Loading/>)}
+            {user ? (<></>) : (<Loading />)}
             <div className='mainProfileTitelBody'>
                 <Button onClick={logout} type="link" style={{ marginTop: '-15px' }} danger>
                     <AiOutlineLogout className='mainProfileTitelIcon' />
@@ -97,28 +97,28 @@ export default function MainProfile() {
             <div className='mainProfileBody'>
                 <div className='mainProfileBox'>
                     <div className='mainProfileImgDiv'>
-                        <img src={user&&user.image?user.image:ProfilePhoto} className='mainProfileImg'>
+                        <img src={user && user.image ? user.image : ProfilePhoto} className='mainProfileImg'>
                         </img>
-                        <div onClick={()=>{
+                        <div onClick={() => {
                             domRef.current.click()
                         }} className='mainProfileIconDiv'>
                             <AiOutlinePaperClip className='mainProfileIcon' />
                         </div>
-                        <input ref={domRef} onChange={(e)=>{
+                        <input ref={domRef} onChange={(e) => {
                             console.log(e.target.files)
-                            fileToDataUri(e.target.files[0]).then(url=>{
+                            fileToDataUri(e.target.files[0]).then(url => {
                                 setFile(url)
                             })
-                            
-                            upload(e.target.files[e.target.files.length-1])
-                        }} style={{display:'none'}} accept='image/jpg' type='file' name='file'></input>
+
+                            upload(e.target.files[e.target.files.length - 1])
+                        }} style={{ display: 'none' }} accept='image/jpg' type='file' name='file'></input>
                     </div>
                 </div>
 
 
                 <div className='mainProfileName'>
-                    
-                    <h2 className='mainProfilebosText'>Name: {user&&user.name?user.name:''}</h2>
+
+                    <h2 className='mainProfilebosText'>Name: {user && user.name ? user.name : ''}</h2>
                 </div>
 
                 <div className='mainProfileName'>
@@ -126,18 +126,18 @@ export default function MainProfile() {
                 </div>
 
                 <div className='mainProfileName'>
-                    <h2 className='mainProfilebosText'>Phone No: {user&&user.phone?user.phone:''}</h2>
+                    <h2 className='mainProfilebosText'>Phone No: {user && user.phone ? user.phone : ''}</h2>
                 </div>
 
                 <div className='mainProfileName'>
-                    <h2 className='mainProfilebosText'>E-mail: {user&&user.email?user.email:''}</h2>
+                    <h2 className='mainProfilebosText'>E-mail: {user && user.email ? user.email : ''}</h2>
                 </div>
 
                 <div className='mainProfileAddress'>
-                    <h2 className='mainProfilebosText'>Address: {user&& user.address?user.address:''}</h2>
+                    <h2 className='mainProfilebosText'>Address: {user && user.address ? user.address : ''}</h2>
                 </div>
             </div>
-            <div style={{marginTop:'20px'}}></div>
+            <div style={{ marginTop: '20px' }}></div>
         </div>
     )
 }
