e19ff73430df9afd1d7d899072f0cf97be8945a7
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-12-06 21:37:21 +0600

    Update Gallery.js

--- src/Card/Gallery.js
+++ src/Card/Gallery.js
@@ -75,7 +75,27 @@ export default function Gallery() {
 
     return (
         <div className='gallerBody'>
-            
+            <Upload style={{
+                width: '350px',
+                height: '350px'
+            }}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                listType="picture-card"
+                fileList={fileList}
+                onPreview={handlePreview}
+                onChange={handleChange}
+            >
+                {fileList.length >= 10 ? null : uploadButton}
+            </Upload>
+            <Modal visible={previewVisible} title={previewTitle} footer={null} onCancel={handleCancel}>
+                <img
+                    alt="example"
+                    style={{
+                        width: '100%',
+                    }}
+                    src={previewImage}
+                />
+            </Modal>
         </div>
     )
 }
