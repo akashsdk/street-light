ca13343f6081f97842d53c7410f6aafccb951260
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-12-06 21:43:22 +0600

    ok

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import "./Index.css";
+import { Col, Row, Pagination } from "antd";
+
 
 export default function Messages() {
+    const [page, setPage] = React.useState(1);
   return (
-    <div>Messages</div>
+    <div className="indecNoticeDiv">
+        <h5>Messages Board</h5>
+        <div className="indecNoticeBox">
+          {page == 1 ? (
+            <p className="indecNoticeText">
+              this website is under maintenance 
+            </p>
+          ) : page == 2 ? (
+            <p className="indecNoticeText">upcoming Messages 1</p>
+          ) : page == 3 ? (
+            <p className="indecNoticeText">upcoming notice 2</p>
+          ) : page == 4 ? (
+            <p className="indecNoticeText">upcoming notice 3</p>
+          ) : page == 5 ? (
+            <p className="indecNoticeText">upcoming notice 4</p>
+          ) : (
+            <p className="indecNoticeText">Error</p>
+          )}
+        </div>
+        <Pagination
+          onChange={(page, pageSize) => {
+            setPage(page);
+          }}
+          defaultCurrent={1}
+          current={page}
+          total={50}
+        />
+      </div>
   )
 }
--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./NoticeBoard.css";
 
 export default function NoticeBoard() {
    
