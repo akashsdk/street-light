e48ced001bf8507816e01ee164d445f80c3a869a
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-12-06 21:41:52 +0600

    Update NoticeBoard.js

--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -1,39 +1,10 @@
 import React from "react";
-import "./Index.css";
-import { Col, Row, Pagination } from "antd";
 
 export default function NoticeBoard() {
-    const [page, setPage] = React.useState(1);
+   
   return (
     <div>
-      <div className="indecNoticeDiv">
-        <h5>Notice Board</h5>
-        <div className="indecNoticeBox">
-          {page == 1 ? (
-            <p className="indecNoticeText">
-              this website is under maintenance 
-            </p>
-          ) : page == 2 ? (
-            <p className="indecNoticeText">upcoming Messages 1</p>
-          ) : page == 3 ? (
-            <p className="indecNoticeText">upcoming notice 2</p>
-          ) : page == 4 ? (
-            <p className="indecNoticeText">upcoming notice 3</p>
-          ) : page == 5 ? (
-            <p className="indecNoticeText">upcoming notice 4</p>
-          ) : (
-            <p className="indecNoticeText">Error</p>
-          )}
-        </div>
-        <Pagination
-          onChange={(page, pageSize) => {
-            setPage(page);
-          }}
-          defaultCurrent={1}
-          current={page}
-          total={50}
-        />
-      </div>
+      NoticeBoard
     </div>
   );
 }
