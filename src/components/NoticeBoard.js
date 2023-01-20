091db56a307ce64541599499b89e2de0ba5b17b4
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-21 03:02:43 +0600

    ok

--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -1,13 +1,44 @@
 import React from "react";
 import "./NoticeBoard.css";
 
+import { Pagination } from "antd";
+import { useState } from "react";
+
 export default function NoticeBoard() {
-   
+  const [current, setCurrent] = useState(3);
+  const [page, setPage] = useState(3);
+
+  const onChange = (page) => {
+    console.log(page);
+    setCurrent(page);
+    setPage(page);
+
+  };
+
+
   return (
     <div className="noticeBody">
       <p className="noticeHedertext">Notice Board</p>
       <div>
-        <div className="noticeBoxLine"/>
+        <div className="noticeBoxLine" />
+
+        {page == 1 ? (
+          <p className="">this website is under maintenance</p>
+        ) : page == 2 ? (
+          <p className="">upcoming Messages 1</p>
+        ) : page == 3 ? (
+          <p className="">upcoming notice 2</p>
+        ) : page == 4 ? (
+          <p className="">upcoming notice 3</p>
+        ) : page == 5 ? (
+          <p className="">upcoming notice 4</p>
+        ) : (
+          <p className="">Error</p>
+        )}
+
+        <Pagination 
+        
+         current={current} onChange={onChange} total={50} />
       </div>
     </div>
   );
